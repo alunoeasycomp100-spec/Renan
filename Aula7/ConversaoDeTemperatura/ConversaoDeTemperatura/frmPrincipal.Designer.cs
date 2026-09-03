5a64d1a42caa74a21877ef0577fac7a637bb0189
alunoeasycomp100-spec <alunoeasycomp100@gmail.com>
2026-09-03 10:30:12 -0400

    Add files via upload

---
+++ Aula7/ConversaoDeTemperatura/ConversaoDeTemperatura/frmPrincipal.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ConversaoDeTemperatura
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.rbCelsius2 = new System.Windows.Forms.RadioButton();
+            this.rbKelvin2 = new System.Windows.Forms.RadioButton();
+            this.rbFahrenheit2 = new System.Windows.Forms.RadioButton();
+            this.gbSaida = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.gbEntrada = new System.Windows.Forms.GroupBox();
+            this.rbCelsius1 = new System.Windows.Forms.RadioButton();
+            this.rbKelvin1 = new System.Windows.Forms.RadioButton();
+            this.rbFahrenheit1 = new System.Windows.Forms.RadioButton();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbSaida.SuspendLayout();
+            this.gbEntrada.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(96, 9);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(413, 26);
+            this.txtValor.TabIndex = 0;
+            // 
+            // rbCelsius2
+            // 
+            this.rbCelsius2.AutoSize = true;
+            this.rbCelsius2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCelsius2.Location = new System.Drawing.Point(79, 27);
+            this.rbCelsius2.Name = "rbCelsius2";
+            this.rbCelsius2.Size = new System.Drawing.Size(71, 24);
+            this.rbCelsius2.TabIndex = 2;
+            this.rbCelsius2.TabStop = true;
+            this.rbCelsius2.Text = "Celsius";
+            this.rbCelsius2.UseVisualStyleBackColor = true;
+            // 
+            // rbKelvin2
+            // 
+            this.rbKelvin2.AutoSize = true;
+            this.rbKelvin2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbKelvin2.Location = new System.Drawing.Point(79, 57);
+            this.rbKelvin2.Name = "rbKelvin2";
+            this.rbKelvin2.Size = new System.Drawing.Size(69, 24);
+            this.rbKelvin2.TabIndex = 2;
+            this.rbKelvin2.TabStop = true;
+            this.rbKelvin2.Text = "Kelvin";
+            this.rbKelvin2.UseVisualStyleBackColor = true;
+            // 
+            // rbFahrenheit2
+            // 
+            this.rbFahrenheit2.AutoSize = true;
+            this.rbFahrenheit2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFahrenheit2.Location = new System.Drawing.Point(79, 87);
+            this.rbFahrenheit2.Name = "rbFahrenheit2";
+            this.rbFahrenheit2.Size = new System.Drawing.Size(98, 24);
+            this.rbFahrenheit2.TabIndex = 2;
+            this.rbFahrenheit2.TabStop = true;
+            this.rbFahrenheit2.Text = "Fahrenheit";
+            this.rbFahrenheit2.UseVisualStyleBackColor = true;
+            // 
+            // gbSaida
+            // 
+            this.gbSaida.Controls.Add(this.rbCelsius2);
+            this.gbSaida.Controls.Add(this.rbKelvin2);
+            this.gbSaida.Controls.Add(this.rbFahrenheit2);
+            this.gbSaida.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbSaida.Location = new System.Drawing.Point(309, 45);
+            this.gbSaida.Name = "gbSaida";
+            this.gbSaida.Size = new System.Drawing.Size(200, 118);
+            this.gbSaida.TabIndex = 2;
+            this.gbSaida.TabStop = false;
+            this.gbSaida.Text = "Saida";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Image = global::ConversaoDeTemperatura.Properties.Resources.Limpar;
+            this.btnLimpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpar.Location = new System.Drawing.Point(318, 204);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(88, 43);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Image = global::ConversaoDeTemperatura.Properties.Resources.Converte;
+            this.btnConverter.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConverter.Location = new System.Drawing.Point(120, 204);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(96, 41);
+            this.btnConverter.TabIndex = 3;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // gbEntrada
+            // 
+            this.gbEntrada.Controls.Add(this.rbCelsius1);
+            this.gbEntrada.Controls.Add(this.rbKelvin1);
+            this.gbEntrada.Controls.Add(this.rbFahrenheit1);
+            this.gbEntrada.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbEntrada.Location = new System.Drawing.Point(43, 45);
+            this.gbEntrada.Name = "gbEntrada";
+            this.gbEntrada.Size = new System.Drawing.Size(200, 118);
+            this.gbEntrada.TabIndex = 1;
+            this.gbEntrada.TabStop = false;
+            this.gbEntrada.Text = "Entrada";
+            // 
+            // rbCelsius1
+            // 
+            this.rbCelsius1.AutoSize = true;
+            this.rbCelsius1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCelsius1.Location = new System.Drawing.Point(79, 27);
+            this.rbCelsius1.Name = "rbCelsius1";
+            this.rbCelsius1.Size = new System.Drawing.Size(71, 24);
+            this.rbCelsius1.TabIndex = 2;
+            this.rbCelsius1.TabStop = true;
+            this.rbCelsius1.Text = "Celsius";
+            this.rbCelsius1.UseVisualStyleBackColor = true;
+            // 
+            // rbKelvin1
+            // 
+            this.rbKelvin1.AutoSize = true;
+            this.rbKelvin1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbKelvin1.Location = new System.Drawing.Point(79, 57);
+            this.rbKelvin1.Name = "rbKelvin1";
+            this.rbKelvin1.Size = new System.Drawing.Size(69, 24);
+            this.rbKelvin1.TabIndex = 2;
+            this.rbKelvin1.TabStop = true;
+            this.rbKelvin1.Text = "Kelvin";
+            this.rbKelvin1.UseVisualStyleBackColor = true;
+            // 
+            // rbFahrenheit1
+            // 
+            this.rbFahrenheit1.AutoSize = true;
+            this.rbFahrenheit1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFahrenheit1.Location = new System.Drawing.Point(79, 87);
+            this.rbFahrenheit1.Name = "rbFahrenheit1";
+            this.rbFahrenheit1.Size = new System.Drawing.Size(98, 24);
+            this.rbFahrenheit1.TabIndex = 2;
+            this.rbFahrenheit1.TabStop = true;
+            this.rbFahrenheit1.Text = "Fahrenheit";
+            this.rbFahrenheit1.UseVisualStyleBackColor = true;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(96, 169);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(413, 26);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Resultado:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 257);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gbEntrada);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbSaida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor de Temperatura";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbSaida.ResumeLayout(false);
+            this.gbSaida.PerformLayout();
+            this.gbEntrada.ResumeLayout(false);
+            this.gbEntrada.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.RadioButton rbCelsius2;
+        private System.Windows.Forms.RadioButton rbKelvin2;
+        private System.Windows.Forms.RadioButton rbFahrenheit2;
+        private System.Windows.Forms.GroupBox gbSaida;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.GroupBox gbEntrada;
+        private System.Windows.Forms.RadioButton rbCelsius1;
+        private System.Windows.Forms.RadioButton rbKelvin1;
+        private System.Windows.Forms.RadioButton rbFahrenheit1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Aula7/ConversaoDeTemperatura/ConversaoDeTemperatura/frmPrincipal.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversaoDeTemperatura
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(txtValor.Text);
+
+            // Entrada: Celsius
+            if (rbCelsius1.Checked == true)
+            {
+                if (rbCelsius2.Checked == true)
+                {
+                    txtResultado.Text = valor + "°C";
+                }
+                else if (rbFahrenheit2.Checked == true)
+                {
+                    txtResultado.Text = (valor * 1.8 + 32) + "°F";
+                }
+                else if (rbKelvin2.Checked == true)
+                {
+                    txtResultado.Text = (valor + 273.15) + " K";
+                }
+            }
+
+            // Entrada: Fahrenheit
+            else if (rbFahrenheit1.Checked == true)
+            {
+                if (rbCelsius2.Checked == true)
+                {
+                    txtResultado.Text = ((valor - 32) / 1.8) + "°C";
+                }
+                else if (rbFahrenheit2.Checked == true)
+                {
+                    txtResultado.Text = valor + "°F";
+                }
+                else if (rbKelvin2.Checked == true)
+                {
+                    txtResultado.Text = (((valor - 32) / 1.8) + 273.15) + " K";
+                }
+            }
+
+            // Entrada: Kelvin
+            else if (rbKelvin1.Checked == true)
+            {
+                if (rbCelsius2.Checked == true)
+                {
+                    txtResultado.Text = (valor - 273.15) + "°C";
+                }
+                else if (rbFahrenheit2.Checked == true)
+                {
+                    txtResultado.Text = (((valor - 273.15) * 1.8) + 32) + "°F";
+                }
+                else if (rbKelvin2.Checked == true)
+                {
+                    txtResultado.Text = valor + " K";
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor.Text = "";
+            txtResultado.Clear(); 
+
+            foreach (Control componente in gbEntrada.Controls)
+            {
+                (componente as RadioButton).Checked = false;
+            }
+
+            foreach (Control componente in gbSaida.Controls)
+            {
+                (componente as RadioButton).Checked = false;
+            }
+        }
+    }
+}
+
+
